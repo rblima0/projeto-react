ce180a84896f03caffbb3303eeb7de8ed5be8c37
Rodrigo Banci <rblima0@gmail.com>
2018-04-11 16:13:53 -0300

    Adicionado servidor local, chamada por Ajax dinamica

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {lista : []}
+  }
+
+  componentDidMount(){
+    $.ajax({
+      url:"https://cdc-react.herokuapp.com/api/autores",
+      dataType: 'json',
+      success:function(resposta){
+        this.setState({lista:resposta});
+      }.bind(this)
+    });
+  }
+
   render() {
     return (
       <div id="layout">
@@ -12,59 +28,75 @@ class App extends Component {
 
         <div id="menu">
             <div className="pure-menu">
-                <a className="pure-menu-heading" href="#">Company</a>
+                <a className="pure-menu-heading" href="#">Sistema</a>
 
                 <ul className="pure-menu-list">
                     <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-
-                    <li className="pure-menu-item menu-item-divided pure-menu-selected">
-                        <a href="#" className="pure-menu-link">Services</a>
-                    </li>
-
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contato</a></li>
                 </ul>
             </div>
         </div>
 
         <div id="main">
             <div className="header">
-                <h1>Page Title</h1>
-                <h2>A subtitle for your page goes here</h2>
+              <h1>Home</h1>
+              <h2>Cadastro de Autores</h2>
             </div>
 
-            <div className="content">
-                <h2 className="content-subhead">How to use this layout</h2>
-                <p>
-                    To use this layout, you can just copy paste the HTML, along with the CSS in <a href="/css/layouts/side-menu.css" alt="Side Menu CSS">side-menu.css</a>, and the JavaScript in <a href="/js/ui.js">ui.js</a>. The JS file uses vanilla JavaScript to simply toggle an <code>active</code> className that makes the menu responsive.
-                </p>
+            <div className="content" id="content">
+              <div className="pure-form pure-form-aligned">
 
-                <h2 className="content-subhead">Now Let's Speak Some Latin</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                <form className="pure-form pure-form-aligned">
+                  <div className="pure-control-group">
+                    <label htmlFor="nome">Nome</label> 
+                    <input className="pure-input-1-2" id="nome" type="text" name="nome" value="" />
+                  </div>
 
-                <div className="pure-g">
-                    <div className="pure-u-1-4">
-                        <img className="pure-img-responsive" src="http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg" alt="Peyto Lake" />
-                    </div>
-                    <div className="pure-u-1-4">
-                        <img className="pure-img-responsive" src="http://farm3.staticflickr.com/2813/9069585985_80da8db54f.jpg" alt="Train" />
-                    </div>
-                    <div className="pure-u-1-4">
-                        <img className="pure-img-responsive" src="http://farm6.staticflickr.com/5456/9121446012_c1640e42d0.jpg" alt="T-Shirt Store" />
-                    </div>
-                    <div className="pure-u-1-4">
-                        <img className="pure-img-responsive" src="http://farm8.staticflickr.com/7357/9086701425_fda3024927.jpg" alt="Mountain" />
-                    </div>
-                </div>
+                  <div className="pure-control-group">
+                    <label htmlFor="email">E-mail</label> 
+                    <input className="pure-input-1-2" id="email" type="email" name="email" value=""  />
+                  </div>
 
-                <h2 className="content-subhead">Try Resizing your Browser</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                  <div className="pure-control-group">
+                    <label htmlFor="senha">Senha</label> 
+                    <input className="pure-input-1-2" id="senha" type="password" name="senha"  />
+                  </div>
+
+                  <div className="pure-control-group">
+                    <label></label> 
+                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                  </div>
+                </form>             
+
+              </div>
+
+              <div>            
+                <table className="pure-table">
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>E-mail</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      this.state.lista.map(function(autor) {
+                        return(
+                          <tr>
+                            <td>{autor.nome}</td>
+                            <td>{autor.email}</td>
+                          </tr>
+                        );
+                      })
+                    }
+                  </tbody>
+                </table> 
+              </div>
+
             </div>
-        </div>
+          </div> 
     </div>
     );
   }
