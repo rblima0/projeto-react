a24b9161f9a62b813258a87becb6e69167e7fcd1
Rodrigo Banci <rblima0@gmail.com>
2018-04-12 18:22:06 -0300

    Refatoramento do cadastro, Publish subscriber e validação

--- cdc-admin/src/componentes/InputCustomizado.js
+++ cdc-admin/src/componentes/InputCustomizado.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
 
 export default class InputCustomizado extends Component {
+
+    constructor(){
+		super();
+		this.state = {msgErro:''};
+    }
+    
     render() {
         return (
             <div className="pure-control-group">
                 <label htmlFor={this.props.id}>{this.props.label}</label> 
-                <input className="pure-input-1-2" id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value} onChange={this.props.onChange}/>
+                <input className="pure-input-1-2" id={this.props.id} type={this.props.type} name={this.props.name} value={this.props.value} onChange={this.props.onChange} />
+                <span className="error">{this.state.msgErro}</span>
             </div>
         );
     }
+
+    componentDidMount() {
+		PubSub.subscribe("erro-validacao",function(topico,erro){			
+			if(erro.field === this.props.name){
+				this.setState({msgErro:erro.defaultMessage});			
+			}
+		}.bind(this));
+
+		PubSub.subscribe("limpa-erros",function(topico){						
+			this.setState({msgErro:''});						
+		}.bind(this));		
+	}
 }
 
 /* props - guarda todos os parametros enviados para esse componente */
