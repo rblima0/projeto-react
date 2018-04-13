493065592fe43eae71e304450c50e10e0403293e
Rodrigo Banci <rblima0@gmail.com>
2018-04-13 15:13:52 -0300

    Usando React Router para navegação

---
+++ cdc-admin/src/Livro.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Livro extends Component {
+    render(){
+        return (
+            <div>
+                <div className="header">
+                    <h1>Cadastro de Livro</h1>
+                </div>
+
+                <div className="content" id="content">
+                    <p>Cadastro aqui...</p>
+                </div>
+            </div>
+        );
+    }
+}
--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorAdmin from './Autor';
+import LivroAdmin from './Livro';
+import Home from './Home';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render((
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/autor" component={AutorAdmin}/>
+                <Route path="/livro" component={LivroAdmin}/>
+            </Switch>
+        </App>
+    </Router>
+
+), document.getElementById('root'));
+//registerServiceWorker();
